7fa43a0de8900f938d5103afe38b191560438996
bohn002 <nath@nbohn.com>
2020-12-08 09:32:38 -0600

    combining

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "arn" {
   description = "The ARN for the Elasticsearch cluster"
-  value       = aws_elasticsearch_domain.es[0].arn
+  value       = coalesce(aws_elasticsearch_domain.es.arn, aws_elasticsearch_domain.es_domain_undestroyable.arn)
 }
 
 output "domain_name" {
   description = "The domain_name for the Elasticsearch cluster"
-  value       = aws_elasticsearch_domain.es[0].domain_name
+  value       = coalesce(aws_elasticsearch_domain.es.domain_name, aws_elasticsearch_domain.es_domain_undestroyable.domain_name)
 }
 
 output "endpoint" {
   description = "The endpoint for the Elasticsearch cluster"
-  value       = aws_elasticsearch_domain.es[0].endpoint
+  value       = coalesce(aws_elasticsearch_domain.es.endpoint, aws_elasticsearch_domain.es_domain_undestroyable.endpoint)
 }
 
 output "kibana_endpoint" {
   description = "The kibana endpoint for the Elasticsearch cluster"
-  value       = aws_elasticsearch_domain.es[0].kibana_endpoint
+  value       = coalesce(aws_elasticsearch_domain.es.kibana_endpoint, aws_elasticsearch_domain.es_domain_undestroyable.kibana_endpoint)
 }
 
 output "log_group_arn" {
