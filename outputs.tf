c1e29e4f3d9f35f06577447abee9fe20d2b72c9b
bohn002 <nath@nbohn.com>
2020-12-08 10:01:41 -0600

    this list flat

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "arn" {
   description = "The ARN for the Elasticsearch cluster"
-  value       = coalesce(aws_elasticsearch_domain.es[*].arn, aws_elasticsearch_domain.es_domain_undestroyable[*].arn)
+  value       = element(compact(concat(aws_elasticsearch_domain.es[*].arn, aws_elasticsearch_domain.es_domain_undestroyable[*].arn)), 0)
 }
 
 output "domain_name" {
   description = "The domain_name for the Elasticsearch cluster"
-  value       = coalesce(aws_elasticsearch_domain.es[*].domain_name, aws_elasticsearch_domain.es_domain_undestroyable[*].domain_name)
+  value       = element(compact(concat(aws_elasticsearch_domain.es[*].domain_name, aws_elasticsearch_domain.es_domain_undestroyable[*].domain_name)), 0)
 }
 
 output "endpoint" {
   description = "The endpoint for the Elasticsearch cluster"
-  value       = coalesce(aws_elasticsearch_domain.es[*].endpoint, aws_elasticsearch_domain.es_domain_undestroyable[*].endpoint)
+  value       = element(compact(concat(aws_elasticsearch_domain.es[*].endpoint, aws_elasticsearch_domain.es_domain_undestroyable[*].endpoint)), 0)
 }
 
 output "kibana_endpoint" {
   description = "The kibana endpoint for the Elasticsearch cluster"
-  value       = coalesce(aws_elasticsearch_domain.es[*].kibana_endpoint, aws_elasticsearch_domain.es_domain_undestroyable[*].kibana_endpoint)
+  value       = element(compact(concat(aws_elasticsearch_domain.es[*].kibana_endpoint, aws_elasticsearch_domain.es_domain_undestroyable[*].kibana_endpoint)), 0)
 }
 
 output "log_group_arn" {
